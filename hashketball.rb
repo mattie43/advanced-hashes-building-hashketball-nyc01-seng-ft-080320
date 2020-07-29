91e47086e26b38b3bacbc4b8e70de692f6dbfb43
Matt Ericksen <ericksenm93@ymail.com>
2020-07-29 23:06:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -28,6 +28,17 @@ def game_hash
         :slam_dunks => 7
         },
         {
+        :player_name => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+        },
+        {
         :player_name => "Mason Plumlee",
         :number => 1,
         :shoe => 19,
