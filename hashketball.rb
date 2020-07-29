23593ab278e1cb12b0ee729bcfdede6ebad12c34
Matt Ericksen <ericksenm93@ymail.com>
2020-07-29 23:03:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,17 +4,19 @@ def game_hash
     :home => 
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{
+      :players => [
+        {
         :player_name => "Alan Anderson",
-        :number => 1,
-        :shoe => 13,
-        :points => 1,
-        :rebounds => 1,
-        :assists => 1,
-        :steals => 1,
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
         :blocks => 1,
         :slam_dunks => 1
-      }]
+        }
+        ]
     :away => 
       :team_name => "name",
       :colors => ["color1", "color2"],
