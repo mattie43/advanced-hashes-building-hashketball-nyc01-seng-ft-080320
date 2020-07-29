7bbd566efcddd96bd97d3d2a0d2aef7a6d1ec0c4
Matt Ericksen <ericksenm93@ymail.com>
2020-07-29 23:02:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,6 +15,19 @@ def game_hash
         :blocks => 1,
         :slam_dunks => 1
       }]
-    :away => away
+    :away => 
+      :team_name => "name",
+      :colors => ["color1", "color2"],
+      :players => [{
+        :player_name => "name",
+        :number => 1,
+        :shoe => 13,
+        :points => 1,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 1
+      }]
   }
 end
